9d51beebc8c33b3eb0a96a8c2c0594df593480b1
mohammad ayesh <mohammad.ayeshh@gmail.com>
2023-04-10 12:28:44 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        int sum = calculateSum(0, 100);
+        System.out.println("The sum of numbers from 0 to 100 is: " + sum);
+//        int sum2 = calculateSum(1, 21, 7);
+//        System.out.println("The sum of numbers divisible by 7 from 1 to 21 is: " + sum2);
+
+
+    }
+
+    public static int calculateSum(int start, int end) {
+        if (start == end) {
+            System.out.println(start);
+            return start;
+        }
+        else {
+            System.out.println(start);
+            return start + calculateSum(start + 1, end);
+        }
+
+    }
+
+    public static int calculateSum(int start, int end, int divisor) {
+        if (start == end) {
+            System.out.println(start);
+            return (start % divisor == 0) ? start : 0;
+        }
+
+        else {
+            int currentSum = (start % divisor == 0) ? start : 0;
+            System.out.println(start);
+            return currentSum + calculateSum(start + 1, end, divisor);
+        }
+    }
+}
+
+
