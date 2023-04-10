8ab72f6286529333dcefdfbd70297350a33d6989
mohammad ayesh <mohammad.ayeshh@gmail.com>
2023-04-10 12:30:13 +0300

    warninqs fix

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+
+
         int sum = calculateSum(0, 100);
         System.out.println("The sum of numbers from 0 to 100 is: " + sum);
-//        int sum2 = calculateSum(1, 21, 7);
-//        System.out.println("The sum of numbers divisible by 7 from 1 to 21 is: " + sum2);
-
+        int sum2 = calculateSum(1, 21, 7);
+        System.out.println("The sum of numbers divisible by 7 from 1 to 21 is: " + sum2);
 
     }
 
